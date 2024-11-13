1d424f9dd7127cc31fe6fd621f59545b9b4b39c9
iraklitsikhelishvilii <11cixela11@gmail.com>
2024-11-13 15:53:31 +0400

    responsive

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let days = prompt("შეიყვანეთ დღე");
+if (typeof days !== "string") {
+  prompt("შეიყვანეთ დღეების რაოდენობა!");
+}
 let box1 = document.getElementById("box1");
 let index = parseInt(days);
 let p = document.createElement("P");
@@ -18,6 +21,9 @@ let interval = setInterval(() => {
 }, 1000 * 60 * 60 * 24);
 
 let hours = prompt("შეიყვანეთ საათი");
+if (hours != typeof Number) {
+  alert("შეიყვანეთ საათბის რაოდენობა");
+}
 let box2 = document.getElementById("box2");
 let index2 = parseInt(hours);
 let p2 = document.createElement("P");
@@ -35,3 +41,47 @@ let interval2 = setInterval(() => {
     clearInterval(interval2);
   }
 }, 1000 * 60 * 60);
+
+let Minutes = prompt("შეიყვანეთ წუთი");
+if (Minutes != typeof Number) {
+  alert("შეიყვანეთ წუთების რაოდენობა");
+}
+let box3 = document.getElementById("box3");
+let index3 = parseInt(Minutes);
+let p3 = document.createElement("p");
+p3.textContent = index3;
+p3.classList.add("p_number");
+box3.appendChild(p3);
+let interval3 = setInterval(() => {
+  box3.textContent = "";
+  let NewP = document.createElement("p");
+  NewP.textContent = index3;
+  NewP.classList.add("p_number");
+  box3.appendChild(NewP);
+  index3--;
+  if (index3 === -1) {
+    clearInterval(interval3);
+  }
+}, 1000 * 60);
+
+let seconds = prompt("შეიყვანეთ წამი");
+if (seconds !== typeof Number) {
+  alert("შეიყვანეთ წამების რაოდენობა");
+}
+let box4 = document.getElementById("box4");
+let index4 = parseInt(seconds);
+let p4 = document.createElement("P");
+p4.textContent = index4;
+p4.classList.add("p_number");
+box4.appendChild(p4);
+let interval4 = setInterval(() => {
+  box4.textContent = "";
+  let NewP = document.createElement("p");
+  NewP.textContent = index4;
+  NewP.classList.add("p_number");
+  box4.appendChild(NewP);
+  index4--;
+  if (index4 === -1) {
+    clearInterval(interval4);
+  }
+}, 1000);
